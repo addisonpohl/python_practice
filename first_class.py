13cfaff6cf4fb2b786dd8722a7323bc8e5d63847
Addison <addison.campbell.pohl@gmail.com>
2021-06-15 23:47:46 -0500

    Class practice

---
+++ first_class.py
@@ -0,0 +1,10 @@
+# Creating a class
+
+class Car:
+    pass
+
+my_car = Car()
+print(my_car)
+print(type(my_car))
+print(isinstance(my_car, Car))
+# returns checks a value and determines if it is or isn't an instance of the provided class. Returns a boolean
